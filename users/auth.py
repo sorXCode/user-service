57149fe75932a9c38239a713a532d0609426deb1
sorxcode <sorxcode@gmail.com>
2021-02-20 11:13:05 +0100

    updated .env vars

--- users/auth.py
+++ users/auth.py
@@ -4,6 +4,6 @@ from fastapi_users.authentication import JWTAuthentication
 
 SECRET = os.environ["SECRET"]
 
-jwt_authentication = JWTAuthentication(secret=SECRET, lifetime_seconds=3600)
+jwt_authentication = JWTAuthentication(secret=SECRET, lifetime_seconds=int(os.envron.get("JWT_LIFETIME",3600)))
 
 auth_backends = [jwt_authentication]
